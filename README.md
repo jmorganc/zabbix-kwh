dfe826a61dd6b97f83166620e2f7eaebf7c7b2fe
Morgan Campbell <jmorgancampbell@gmail.com>
2015-02-01 21:15:33 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zabbix-kwh
+zabbix_sender -z <host> -i IntervalMeterUsage_<int>.txt -T -v
---
+++ parse_meter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import time
+
+
+def main():
+    FILES = ('15m', 'daily', 'monthly')
+    ZABBIX_HOST = 'Bespin'
+
+    for file in FILES:
+        with open('IntervalMeterUsage_{file}.csv'.format(file=file), 'rb') as fh:
+            meter_csv_obj = csv.reader(fh)
+            meter_csv_obj.next()
+            with open('IntervalMeterUsage_{file}.txt'.format(file=file), 'w') as txt_out:
+                if file == '15m':
+                    for esiid, date, timestart, timeend, kwh, estact, congen in meter_csv_obj:
+                        timestamp = time.mktime(time.strptime(' '.join((date, timestart)), '%Y-%m-%d %H:%M'))
+                        txt_out.write('Bespin house.kwh.{file} {timestamp} {kwh}\n'.format(file=file, timestamp=timestamp, kwh=kwh))
+                elif file == 'daily':
+                    for esiid, date, reading_start, reading_end, kwh in meter_csv_obj:
+                        timestamp = time.mktime(time.strptime(date, '%m/%d/%Y'))
+                        txt_out.write('Bespin house.kwh.{file} {timestamp} {kwh}\n'.format(file=file, timestamp=timestamp, kwh=kwh))
+                elif file == 'monthly':
+                    for esiid, date_start, date, kwh, kwh_m, kwh_b, kva_m, kva_b in meter_csv_obj:
+                        timestamp = time.mktime(time.strptime(date, '%m/%d/%Y'))
+                        txt_out.write('Bespin house.kwh.{file} {timestamp} {kwh}\n'.format(file=file, timestamp=timestamp, kwh=kwh))
+                else:
+                    sys.exit()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
